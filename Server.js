2cdf1f3144d97cd01214e374e5f0bba6a91e1ada
Yosef-Perelman <yosefper@gmail.com>
2024-09-11 14:09:57 +0300

    Fixed the code.

--- Server.js
+++ Server.js
@@ -19,118 +19,99 @@ const io = socketIo(server, {
     }
 });
 
+
+// 'contents' is a dictionary of codes of the code blocks
 let contents = {};
+// 'room' is a dictionary of users who are in a certain room
 let rooms = {}
+// 'mentors' is dictionary of the mentor of every open room
 let mentors = {};
 
-function getRoomSize(roomName) {
-    return rooms[roomName] ? rooms[roomName].length : 0;
-}
 
-// API to fetch content from MongoDB when a client joins a room
-async function fetchRoomContent(roomName) {
-    console.log(`Attempting to fetch content for room: ${roomName}`);
+function getRoomSize(roomName) { return rooms[roomName] ? rooms[roomName].length : 0; }
+
+async function getDataFromMongo(roomName) {
     try {
-        console.log(`Executing database query for room: ${roomName}`);
+        console.log('Trying to pull information from mongo');
         const content = await Content.findOne({ title: roomName });
-        console.log(`Database query completed for room: ${roomName}`);
 
         if (content) {
-            console.log(`Content found for room ${roomName}:`, content);
             contents[roomName] = {
                 initialCode: content.initialCode,
                 solution: content.solution
             };
             return contents[roomName];
         } else {
-            console.log(`No content found for room: ${roomName}`);
+            console.log('Didn\'t find the info in mongo. Check your spelling and database.' );
             return null;
         }
     } catch (err) {
-        console.error(`Error in fetchRoomContent for room ${roomName}:`, err);
-        console.error(`Error stack trace:`, err.stack);
         throw err;
     }
 }
 
-// Function to reset room content
-function resetRoomContent(roomName) {
-    console.log(`Resetting content for room: ${roomName}`);
+function originalRoomContent(roomName) {
+    console.log('Removing the content from the dict because the room closed.');
     delete contents[roomName];
     delete rooms[roomName];
     delete mentors[roomName];
 }
 
-io.on('connection', (socket) => {
-    console.log(`New client connected ${socket.id}`);
 
-    socket.on('join', async (room_name) => {
-        console.log(`room name = ${room_name}`);
-        socket.join(room_name);
+// Socket operations
+io.on('connection', (socket) => {
+    console.log(`New client connected: ${socket.id}`);
+
+    socket.on('join', async (roomName) => {
+        console.log(`\'join\' func activated. The room name is ${roomName}`);
+        socket.join(roomName);
 
         let isMentor = false;
-        if (!rooms[room_name]) {
-            rooms[room_name] = [];
-            mentors[room_name] = socket.id;
+        if (!rooms[roomName]) {
+            rooms[roomName] = [];
+            mentors[roomName] = socket.id;
             isMentor = true;
         }
-        rooms[room_name].push(socket.id);
+        rooms[roomName].push(socket.id);
 
-        const roomSize = getRoomSize(room_name);
+        const roomSize = getRoomSize(roomName);
 
-        // Fetch room content from MongoDB if not already in memory
-        if (!contents[room_name]) {
-            console.log(`Fetching content for room: ${room_name}`);
+        if (!contents[roomName]) {
             try {
-                const roomContent = await fetchRoomContent(room_name);
+                const roomContent = await getDataFromMongo(roomName);
                 if (roomContent) {
-                    console.log(`Content successfully fetched for room: ${room_name}`);
                     socket.emit('updateContent', roomContent.initialCode);
                 } else {
-                    console.log(`No content found for room: ${room_name}`);
                     socket.emit('updateContent', 'No content found for this room.');
                 }
             } catch (error) {
-                console.error(`Error handling join for room ${room_name}:`, error);
                 socket.emit('updateContent', 'An error occurred while fetching room content.');
             }
         } else {
-            // Send the current content to the newly connected client
-            socket.emit('updateContent', contents[room_name].initialCode);
-            console.log('Sending cached content to new client:', contents[room_name].initialCode);
+            socket.emit('updateContent', contents[roomName].initialCode);
         }
 
-
-        // Notify the new user about the current room size
+        // Updates the new user with the current content of the editor
         socket.emit('roomInfo', { roomSize, isMentor });
 
-        // Notify other users in the room about the new join
-        socket.to(room_name).emit('userJoined', { socketId: socket.id, roomSize });
-
-        console.log(`Number of users in ${room_name}:`, roomSize);
-
+        // Informs the other users that a new user has joined the room
+        socket.to(roomName).emit('userJoined', { socketId: socket.id, roomSize });
     })
 
-    // Send the current content to the newly connected client
-    // socket.emit('updateContent', currentContent);
-    // console.log('Sending current content to new client:', currentContent);
+    socket.on('edit', (content, roomName) => {
+        if (contents[roomName]) {
+            contents[roomName].initialCode = content;
+            io.to(roomName).emit('updateContent', content);
 
-    socket.on('edit', (content, room_name) => {
-        if (contents[room_name]) {
-            contents[room_name].initialCode = content;
-            console.log('Content updated for room:', room_name, content);
-            io.to(room_name).emit('updateContent', content);
-
-            // Check if the content matches the solution
-            if (content === contents[room_name].solution) {
-                console.log(`User in room ${room_name} found the solution!`);
-                io.to(room_name).emit('correctSolution');  // Notify all users
+            if (content === contents[roomName].solution) {
+                console.log('Some user found the solution.');
+                io.to(roomName).emit('correctSolution');
             }
         }
     });
 
     socket.on('disconnect', () => {
-        console.log(`Client disconnected: ${socket.id}`);
+        console.log(`User ${socket.id} left the room.`);
         for (let roomName in rooms) {
             const index = rooms[roomName].indexOf(socket.id);
             if (index !== -1) {
@@ -138,15 +119,14 @@ io.on('connection', (socket) => {
                 const roomSize = getRoomSize(roomName);
 
                 if (mentors[roomName] === socket.id) {
-                    // Mentor has left, notify all users to leave
+                    console.log('The user that left is the mentor. All the students moving to the home page.');
                     io.to(roomName).emit('mentorLeft');
-                    resetRoomContent(roomName);
+                    originalRoomContent(roomName);
                 } else {
                     socket.to(roomName).emit('userLeft', { socketId: socket.id, roomSize });
 
-                    // Check if the room is now empty
                     if (roomSize === 0) {
-                        resetRoomContent(roomName);
+                        originalRoomContent(roomName);
                     }
                 }
             }
@@ -154,10 +134,10 @@ io.on('connection', (socket) => {
     });
 });
 
+
+// Configuration
 const PORT = 5000;
 server.listen(PORT, () => console.log(`Server running on port ${PORT}`));
-
-// Connect to MongoDB
 mongoose.connect(process.env.MONGODB_URL, {}).then(() => {
     console.log('MongoDB connected');
 }).catch((err) => {
--- schemas/Content.js
+++ schemas/Content.js
@@ -1,9 +1,9 @@
 const mongoose = require('mongoose');
 
 const contentSchema = new mongoose.Schema({
-    title: { type: String, required: true },  // Room name
-    initialCode: { type: String, required: true },  // Initial code
-    solution: { type: String, required: true }  // Solution code
+    title: { type: String, required: true },
+    initialCode: { type: String, required: true },
+    solution: { type: String, required: true }
 }, { collection: 'code' });
 
 const Content = mongoose.model('Content', contentSchema);
